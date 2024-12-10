b10d317ff9a463844815a6ab5f0a08fa2ee20348
Ernesto Cobos <ernesto@cobos.io>
2024-12-10 11:23:24 -0600

    Revert "feat(env): add @next/env for environment variable management"
    This reverts commit edc947c1d2139bf8bded3a08a100eeae28c18ebb.

--- misc/axiosInstance.ts
+++ misc/axiosInstance.ts
@@ -1,10 +1,9 @@
-import axios, {AxiosInstance} from 'axios';
-import '@/misc/envConfig'
+import axios, { AxiosInstance } from 'axios';
 
 const api: AxiosInstance = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000',
-    withCredentials: true, // necesario para que las cookies se envíen en las peticiones
-    withXSRFToken: true
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000',
+  withCredentials: true, // necesario para que las cookies se envíen en las peticiones
+  withXSRFToken: true
 });
 
 export default api;
